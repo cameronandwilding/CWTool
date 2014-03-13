196f3c308a4b7289179ddd5c43e8c7e38591b614
michael <michael@michael-Vostro-260.(none)>
2014-03-13 17:21:40 +0000

    added function from interface

--- drupal-8.x-dev/modules/custom/checklist/lib/Drupal/checklist/Controller/ChecklistController.php
+++ drupal-8.x-dev/modules/custom/checklist/lib/Drupal/checklist/Controller/ChecklistController.php
@@ -7,13 +7,15 @@
 
 
 namespace Drupal\checklist\Controller;
+use Drupal\Core\Config\Entity\ConfigEntityListController;
+use Drupal\Core\Entity\EntityListControllerInterface;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Controller\ControllerBase;
 
 
 
-/**
- * Controller routines for book routes.
- */
-class ChecklistController {
+
+class ChecklistController extends ControllerBase {
 
   public function getFormID() {
     return 'checklist_settings';
@@ -22,6 +24,24 @@ class ChecklistController {
 
   public function checklistRender() {
 
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildHeader() {
+    $header['label'] = $this->t('Example');
+    $header['id'] = $this->t('Machine name');
+    return $header + parent::buildHeader();
+  }
+  /**
+   * {@inheritdoc}
+   */
+  public function buildRow(EntityInterface $entity) {
+    $row['label'] = $this->getLabel($entity);
+    $row['id'] = $entity->id();
+    // You probably want a few more properties here...
+    return $row + parent::buildRow($entity);
   }
 
 }
