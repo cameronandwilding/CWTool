395416e2a98e1758a4293ad53331bf42d66a16f6
Peter Arato <it.arato@gmail.com>
2014-10-26 19:32:13 +0000

    Fix dynamic node class loader.

--- src/CW/Controller/AbstractEntityController.php
+++ src/CW/Controller/AbstractEntityController.php
@@ -7,13 +7,28 @@ namespace CW\Controller;
 
 use CW\Model\EntityModel;
 
+/**
+ * Class AbstractEntityController
+ * @package CW\Controller
+ *
+ * Abstraction for entity controller. Contains the model and should be extended
+ * for content specific behaviors.
+ */
 abstract class AbstractEntityController {
 
   /**
+   * The model.
+   *
    * @var \CW\Model\EntityModel
    */
   protected $entityModel;
 
+  /**
+   * Constructor.
+   *
+   * @param \CW\Model\EntityModel $entityModel
+   *  The model object.
+   */
   public function __construct(EntityModel $entityModel) {
     $this->entityModel = $entityModel;
   }
--- src/CW/Controller/NodeControllerFactory.php
+++ src/CW/Controller/NodeControllerFactory.php
@@ -1,27 +1,44 @@
 <?php
 /**
  * @file
+ *
+ * Node controller factory.
  */
 
 namespace CW\Controller;
 
 use CW\Model\EntityModel;
+use CW\Util\EntityLocalProcessIdentityMap;
 
+/**
+ * Class NodeControllerFactory
+ * @package CW\Controller
+ *
+ * The purpose of this class to create
+ */
 class NodeControllerFactory {
 
-  private $baseClass;
+  private $defaultEntityControllerClass;
 
   private $controllerClassMap = array();
 
-  public function __construct($baseClass) {
-    if (!is_subclass_of($baseClass, 'CW\Controller\AbstractEntityController')) {
-      throw new \InvalidArgumentException('Class is not subclass of AbstractEntityController: ' . $baseClass);
+  /**
+   * @var \CW\Util\EntityLocalProcessIdentityMap
+   */
+  private $entityModelLoader;
+
+  public function __construct(EntityLocalProcessIdentityMap $entityModelLoader, $defaultEntityControllerClass) {
+    if (!is_subclass_of($defaultEntityControllerClass, 'CW\Controller\AbstractEntityController')) {
+      throw new \InvalidArgumentException('Class is not subclass of AbstractEntityController: ' . $defaultEntityControllerClass);
     }
 
-    $this->baseClass = $baseClass;
+    $this->defaultEntityControllerClass = $defaultEntityControllerClass;
+
+    $this->entityModelLoader = $entityModelLoader;
 
     $args = func_get_args();
-    array_shift($args);
+    array_shift($args); // Remove entity model loader.
+    array_shift($args); // Remove base class.
     if (count($args) > 0) {
       while (count($args)) {
         $contentTypeName = array_shift($args);
@@ -32,17 +49,15 @@ class NodeControllerFactory {
   }
 
   public function get($nid) {
-    $container = cw_tool_get_container();
-    $loader = $container->get('entity-loader');
     /** @var EntityModel $node */
-    $node = $loader->getFromEntityID('node', $nid);
+    $node = $this->entityModelLoader->getFromEntityID('node', $nid);
 
     $nodeType = $node->getDrupalEntityData()->type;
     if (array_key_exists($nodeType, $this->controllerClassMap)) {
       $controllerClass = $this->controllerClassMap[$nodeType];
     }
     else {
-      $controllerClass = $this->baseClass;
+      $controllerClass = $this->defaultEntityControllerClass;
     }
     $controller = new $controllerClass($node);
 
