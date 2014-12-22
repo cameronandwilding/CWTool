4ab58800f153298642fba8e6388652cb745287d0
Peter Arato <it.arato@gmail.com>
2014-12-22 16:55:38 +0000

    Add batch saver test.

---
+++ tests/phpunit/EntityBatchSaverTest.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * @file
+ *
+ * Entity batch saver test.
+ */
+
+use CW\Util\LocalProcessIdentityMap;
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+class EntityBatchSaverTest extends PHPUnit_Framework_TestCase {
+
+  protected $objectLoaderMock;
+
+  protected $entityBatchSaver;
+
+  protected $entities = array();
+
+  public function setUp() {
+    parent::setUp();
+
+    $identityMap = new LocalProcessIdentityMap();
+    $this->entityBatchSaver = new \CW\Util\EntityBatchSaver($identityMap);
+
+    $entityType = md5(microtime(TRUE));
+    $entityId1 = rand(1, 1000);
+    $entityId2 = rand(1, 1000);
+
+    $this->objectLoaderMock = $this->getMock('CW\\Model\\ObjectLoader');
+    $this->entities[0] = new \CW\Model\EntityModel($this->objectLoaderMock, $entityType, $entityId1);
+    $this->entities[1] = new \CW\Model\EntityModel($this->objectLoaderMock, $entityType, $entityId2);
+
+    $identityMap->add($entityId1, $this->entities[0]);
+    $identityMap->add($entityId2, $this->entities[1]);
+  }
+
+  public function testBatchSaveNothing() {
+    $this->objectLoaderMock
+      ->expects($this->exactly(0))
+      ->method('save');
+
+    $this->entityBatchSaver->saveAll();
+  }
+
+  public function testBatchSaveOne() {
+    $this->objectLoaderMock
+      ->expects($this->exactly(1))
+      ->method('save');
+
+    $this->entities[0]->setDirty();
+
+    $this->entityBatchSaver->saveAll();
+  }
+
+  public function testBatchSaveMultiple() {
+    $this->objectLoaderMock
+      ->expects($this->exactly(2))
+      ->method('save');
+
+    $this->entities[0]->setDirty();
+    $this->entities[1]->setDirty();
+
+    $this->entityBatchSaver->saveAll();
+  }
+
+}
