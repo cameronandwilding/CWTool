12d42aa5c8270b65b21f168e2af680e3dd0b2876
Peter Arato <it.arato@gmail.com>
2014-12-23 18:25:14 +0000

    Add basic controller test.

---
+++ tests/phpunit/Controller/BasicEntityControllerTest.php
@@ -0,0 +1,53 @@
+<?php
+use CW\Controller\BasicEntityController;
+use CW\Model\EntityModel;
+
+/**
+ * @file
+ */
+
+class BasicEntityControllerTest extends PHPUnit_Framework_TestCase {
+
+  /**
+   * @var PHPUnit_Framework_MockObject_MockObject
+   */
+  protected $objectLoaderMock;
+
+  /**
+   * @var BasicEntityController
+   */
+  protected $controller;
+
+  /**
+   * @var PHPUnit_Framework_MockObject_MockObject
+   */
+  protected $entityModelMock;
+
+  public function setUp() {
+    $loaderMock = $this->getMock('CW\Model\DrupalObjectLoader');
+    $this->entityModelMock = $this->getMock('CW\Model\EntityModel', [], [$loaderMock, 'fake type', 'fake id']);
+    $this->controller = new BasicEntityController($this->entityModelMock);
+  }
+
+  public function testLoadModel() {
+    $this->assertEquals(
+      $this->entityModelMock,
+      $this->controller->getEntityModel()
+    );
+  }
+
+  public function testLoadDrupalData() {
+    $this->entityModelMock
+      ->expects($this->once())
+      ->method('getEntityData');
+    $this->controller->data();
+  }
+
+  public function testLoadMetaData() {
+    $this->entityModelMock
+      ->expects($this->once())
+      ->method('getEntityMetadataWrapper');
+    $this->controller->metadata();
+  }
+
+}
