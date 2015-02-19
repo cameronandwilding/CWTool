df68311ae9c1f7b8b2623c1739f3587276c385a9
Peter Arato <it.arato@gmail.com>
2015-02-19 16:16:28 +0000

    Add user login.

--- src/CW/Controller/UserController.php
+++ src/CW/Controller/UserController.php
@@ -35,4 +35,25 @@ class UserController extends AbstractEntityController {
     return 'user';
   }
 
+  public function login() {
+    global $user;
+    // Override global user.
+    $user = $this->entity();
+
+    $this->logger->info(__METHOD__ . ' session opened for {name}.', array('name' => $user->name));
+
+    // Update the user table timestamp noting user has logged in.
+    // This is also used to invalidate one-time login links.
+    $user->login = REQUEST_TIME;
+    db_update('users')
+      ->fields(array('login' => $user->login))
+      ->condition('uid', $user->uid)
+      ->execute();
+
+    // Regenerate the session ID to prevent against session fixation attacks.
+    // This is called before hook_user in case one of those functions fails
+    // or incorrectly does a redirect which would leave the old session in place.
+    drupal_session_regenerate();
+  }
+
 }
