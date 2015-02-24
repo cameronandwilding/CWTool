1913cadcf5213345632b1e303af9614b30d2d6f3
Peter Arato <it.arato@gmail.com>
2015-02-24 15:08:37 +0000

    Add link and update date.

--- src/CW/Util/DateUtil.php
+++ src/CW/Util/DateUtil.php
@@ -10,6 +10,8 @@ use DateTimeZone;
 
 class DateUtil {
 
+  const DAY_IN_SECONDS = 86400;
+
   public static function getTimestampFromDateFieldValue($value) {
     $dateRaw = $value['value'];
     $date = new DateTime($dateRaw, new DateTimeZone($value['timezone']));
---
+++ src/CW/Util/Link.php
@@ -0,0 +1,123 @@
+<?php
+/**
+ * @file
+ */
+
+namespace CW\Util;
+
+class Link {
+
+  /**
+   * @var null
+   */
+  private $path;
+
+  /**
+   * @var null
+   */
+  private $text;
+
+  /**
+   * @var array
+   */
+  private $query;
+
+  /**
+   * @var null
+   */
+  private $fragment;
+
+  /**
+   * @var bool
+   */
+  private $absolute;
+
+  /**
+   * @param null $path
+   * @param null $text
+   * @param array $query
+   * @param null $fragment
+   * @param bool $absolute
+   */
+  public function __construct($path = NULL, $text = NULL, array $query = array(), $fragment = NULL, $absolute = FALSE) {
+    $this->path = $path;
+    $this->text = $text;
+    $this->query = $query;
+    $this->fragment = $fragment;
+    $this->absolute = $absolute;
+  }
+
+  /**
+   * @param $path
+   * @param $fragment
+   * @return Link
+   */
+  public static function withPathAndFragment($path, $fragment) {
+    return new static($path, NULL, array(), $fragment);
+  }
+
+  public function getDrupalURL() {
+    return url($this->path, $this->getDrupalURLOptions());
+  }
+
+  public function getDrupalLink() {
+    return l($this->text, $this->path, $this->getDrupalURLOptions());
+  }
+
+  /**
+   * @param null $path
+   * @return $this
+   */
+  public function setPath($path) {
+    $this->path = $path;
+    return $this;
+  }
+
+  /**
+   * @param null $text
+   * @return $this
+   */
+  public function setText($text) {
+    $this->text = $text;
+    return $this;
+  }
+
+  /**
+   * @param array $query
+   * @return $this
+   */
+  public function setQuery($query) {
+    $this->query = $query;
+    return $this;
+  }
+
+  /**
+   * @param null $fragment
+   * @return $this
+   */
+  public function setFragment($fragment) {
+    $this->fragment = $fragment;
+    return $this;
+  }
+
+  /**
+   * @param boolean $absolute
+   * @return $this
+   */
+  public function setAbsolute($absolute) {
+    $this->absolute = $absolute;
+    return $this;
+  }
+
+  /**
+   * @return array
+   */
+  private function getDrupalURLOptions() {
+    return array(
+      'query' => $this->query,
+      'fragment' => $this->fragment,
+      'absolute' => $this->absolute,
+    );
+  }
+
+}
