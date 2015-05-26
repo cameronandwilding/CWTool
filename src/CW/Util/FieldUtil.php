a66673f6e1955fc4480db0f366877675469c000d
Administrator <administrator@Administrators-MacBook-Air.local>
2015-05-27 00:04:40 +0100

    Added more const fields

--- src/CW/Util/FieldUtil.php
+++ src/CW/Util/FieldUtil.php
@@ -17,5 +17,8 @@ class FieldUtil {
   const KEY_TARGET_ID = 'target_id';
   const KEY_VALUE = 'value';
   const KEY_FILE_ID = 'fid';
+  const KEY_FILENAME = 'filename';
+  const KEY_NODE_ID = 'nid';
+  const KEY_TID = 'tid';
 
 }
