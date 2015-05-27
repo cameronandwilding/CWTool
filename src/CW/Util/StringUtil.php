5a53458d7b402532112d5b46c68e186b9e1b83a3
Eric Smith <eric.smith@cameronandwilding.com>
2015-05-27 12:07:00 +0100

    Added string utils

--- src/CW/Util/StringUtil.php
+++ src/CW/Util/StringUtil.php
@@ -20,4 +20,26 @@ class StringUtil {
   public static function snakeCase($string) {
     return preg_replace('/[^a-zA-Z0-9]/', '_', $string);
   }
+
+  /**
+   * Check that a string ends with another string.
+   *
+   * @param string $string
+   * @param string $needle
+   * @return bool
+   */
+  public static function stringEndsWith($string, $needle) {
+    $offset = strlen($string) - strlen($needle);
+    return strpos($string, $needle, $offset) !== FALSE;
+  }
+
+  /**
+   * Remove the last character from a string.
+   *
+   * @param string $string
+   * @return string
+   */
+  public static function removeLastCharacterFromString($string) {
+    return substr($string, 0, -1);
+  }
 }
