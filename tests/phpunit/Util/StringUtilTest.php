39da4ec35e8e2bd345b9595508cb6a32106f4278
Peter Arato <it.arato@gmail.com>
2015-07-28 16:03:10 +0100

    Add string util and make template vars a ref getter.

--- tests/phpunit/Util/StringUtilTest.php
+++ tests/phpunit/Util/StringUtilTest.php
@@ -21,4 +21,27 @@ class StringUtilTest extends TestCase {
     }
   }
 
+  public function testInsertTo() {
+    $tests = [
+      ['foo', 'bar', 0, 'barfoo'],
+      ['foo', 'bar', 1, 'fbaroo'],
+      ['”“foo', 'bar', 6, '”“barfoo'], // Multibyte warning.
+      ['foo', 'bar', 3, 'foobar'],
+      ['foo', 'bar', -1, 'fobaro'],
+      ["foo\nfoo", 'bar', 3, "foobar\nfoo"], // New lines.
+      ["foo\nfoo", 'bar', 4, "foo\nbarfoo"],
+    ];
+
+    foreach ($tests as $test) {
+      $this->assertEquals($test[3], StringUtil::insertTo($test[0], $test[1], $test[2]));
+    }
+  }
+
+  /**
+   * @expectedException CW\Exception\CWException
+   */
+  public function testInsertToOutOfRange() {
+    StringUtil::insertTo('foo', 'bar', 100);
+  }
+
 }
