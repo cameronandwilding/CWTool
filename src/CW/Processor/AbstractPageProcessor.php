df58cb9fd279fc301f7343a47f1bbccf44b46a89
Eric Smith <eric.smith@cameronandwilding.com>
2015-08-28 12:27:22 +0100

    Updated page processor

---
+++ src/CW/Processor/AbstractPageProcessor.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @file
+ *
+ * Abstract page processor.
+ */
+
+namespace CW\Processor;
+
+/**
+ * Class AbstractPageProcessor
+ * @package CW\Processor
+ *
+ * Responsibility is to handle pre/post process hooks for pages.
+ */
+abstract class AbstractPageProcessor extends AbstractThemeProcessor {
+
+  // Template vars
+  const VAR_IS_FRONT_PAGE = 'is_front';
+  const VAR_ACTION_LINKS = 'action_links';
+  const VAR_TABS = 'tabs';
+
+  /**
+   * @return bool
+   */
+  public function isFrontPage() {
+    return (bool) $this->getVar(self::VAR_IS_FRONT_PAGE);
+  }
+}
