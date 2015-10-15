0271d5a47e431bc950512e434e43995f8955f19f
Peter Arato <it.arato@gmail.com>
2015-10-15 12:26:23 +0800

    Add uri field.

--- src/CW/Util/FieldUtil.php
+++ src/CW/Util/FieldUtil.php
@@ -14,11 +14,13 @@ namespace CW\Util;
 class FieldUtil {
 
   // Field DB table keys.
-  const KEY_TARGET_ID = 'target_id';
   const KEY_VALUE = 'value';
-  const KEY_FILE_ID = 'fid';
-  const KEY_TAXONOMY_ID = 'tid';
-  const KEY_FILENAME = 'filename';
-  const KEY_NODEREFERENCE_ID = 'nid';
 
+  const KEY_TARGET_ID = 'target_id';
+  const KEY_NODEREFERENCE_ID = 'nid';
+  const KEY_TAXONOMY_ID = 'tid';
+
+  const KEY_FILE_ID = 'fid';
+  const KEY_FILENAME = 'filename';
+  const KEY_FILE_URI = 'uri';
 }
